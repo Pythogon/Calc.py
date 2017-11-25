589de9ef1a4a927619d9cbe953e8de5241143b2a
iiSkyii <31135263+iiSkyii@users.noreply.github.com>
2017-11-25 20:00:26 +0000

    First go at Calc.py

---
+++ Calc.py
@@ -0,0 +1,15 @@
+import time
+print("""
+
+=====+++=====
+ Welcome to
+  Calc.py!
+=====+++=====
+
+""")
+while True:
+    time.sleep(1)
+    inp = input("Please enter an equation to calculate! Use 'ans' for last time's answer")
+    ans = eval(inp)
+    time.sleep(1)
+    print(inp,"=",ans)
